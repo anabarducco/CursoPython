f086c05475ed24123a6ea99e614024c99c39d8d4
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-10-21 10:08:09 -0300

    Atualização do exercício com valor do euro)

--- ex010.py
+++ ex010.py
@@ -1,4 +1,4 @@
 #Faça um programa que leia quanto uma pessoa tem na carteira e mostre quantos dolares ela pode comprar
 
 reais = float(input('Insira quantos reais você tem na carteira: '))
-print(f'Você pode comprar {reais*0.19:.2f} dólares com esse valor!')
+print(f'Você pode comprar {reais*0.19:.2f} dólares ou {reais*0.195:.2f} euros com esse valor!')
