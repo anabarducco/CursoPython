4f7be48051daf2d14c78785bfffc73de434301bf
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-10-21 13:01:41 -0300

    Atualização do exercício com duas casas decimais

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
 
 preco = float(input('Insira o preço do produto: '))
-print(f'Com 5% de desconto, o novo valor do produto passa a ser {preco-(preco*0.05)}.')
+print(f'Com 5% de desconto, o novo valor do produto passa a ser {preco-(preco*0.05):.2f}.')
