89b6223527bcaf349151813615e88d0bc4e1b356
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-12-01 09:37:29 -0300

    Correção Exercício 93 (usando lista para armazenar os gols de cada partida)

--- ex093.py
+++ ex093.py
@@ -4,16 +4,26 @@ Depois vai ler a quantidade de gols feitas em cada partida.
 No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato. '''
 
 jogador = dict()
-qtdGols = 0
+gols = list()
 
 jogador['Nome'] = str(input('Nome do Jogador: ')).title()
 jogador['Quantidade de Partidas'] = int(input('Quantidade de Partidas Jogadas: '))
 
 for contagem in range(0, jogador['Quantidade de Partidas']):
-    jogador[f'Quantidade Gols Partida {contagem+1}'] = int(input(f'Quantidade de Gols na Partida {contagem+1}: '))
-    qtdGols += jogador[f'Quantidade Gols Partida {contagem+1}']
-jogador['Quantidade Gols Campeonato'] = qtdGols
+    golPartida = int(input(f'Quantidade de Gols na Partida {contagem+1}: '))
+    gols.append(golPartida)
+jogador['Quantidade Gols Partidas'] = gols
+jogador['Quantidade Gols Campeonato'] = sum(gols)
 
-print(20*'--')
+print(40*'--')
+print(jogador)
+print(40*'--')
 for chave, valor in jogador.items():
-    print(f'{chave}: {valor}')
+    print(f'O campo {chave} tem valor {valor}.')
+print(40*'--')
+print(f'O jogador {jogador["Nome"]} jogou {jogador["Quantidade de Partidas"]} partidas:')
+contagem = 1
+for gol in gols:
+    print(f'    => Na {contagem}ª partida, fez {gol} gols.')
+    contagem += 1
+print(f'Foi um total de {jogador["Quantidade Gols Campeonato"]} gols.')
