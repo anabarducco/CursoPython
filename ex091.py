3c3ca0e3f35a12ee435dc6af98e8f10abc11d0ce
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-12-01 08:40:06 -0300

    Correção exercício 91 com melhor visualização dos resultados

--- ex091.py
+++ ex091.py
@@ -6,14 +6,19 @@ import random
 import time
 
 resultados = dict() #cria dicionário resultados
+for contagem in range(1, 5): #para 4 jogadores
+    resultados[f'Jogador{contagem}'] = random.randint(1, 6) #cria chave jogador[contagem] - valor número aleatório gerado
+
+print(20 * '--')
+print('Os resultados obtidos por cada jogador foram:')
+for chave, valor in resultados.items():
+    time.sleep(1.0)
+    print(f'{chave} tirou {valor} no dado.') #printa a chave (jogador) e o valor (número do dado)
 
 print(20*'--')
-print('Os resultados obtidos por cada jogador foram:')
-for contagem in range(0, 4): #para 4 jogadores
-    resultados[f'Jogador{contagem+1}'] = random.randint(1, 7) #cria chave jogador[contagem] - valor número aleatório gerado
-print(resultados) #printa o dicionário
-print(20*'--')
-print('A colocação de acordo com o resultado obtido fica: ')
+print('Ranking dos Jogadores: ')
+contagem = 1
 for resultado in sorted(resultados, key = resultados.get, reverse=True): #para cada resultado no dicionário resultados, ordena os valores gerados do maior para o menor
     time.sleep(1.0)
-    print(resultado,': ', resultados[resultado])
+    print(f'{contagem}º Lugar: {resultado} - {resultados[resultado]}') #printa o ranking com colocação, jogador e valor do dado
+    contagem += 1
