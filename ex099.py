e395bd5abf432fdd2b431a905f243371483f7cb6
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-12-02 14:05:53 -0300

    Correção Exercício 99 com possibilidade de não passar nenhum valor

--- ex099.py
+++ ex099.py
@@ -1,15 +1,28 @@
 ''' Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior. '''
+import time
 
 
-def maior(*num):
-    print(f'Os valores passados foram: {num}')
-    print(f'Destes {len(num)} valores, o maior é {max(num)}.')
+def maior(* num):
+    time.sleep(0.5)
+    print(30*'-')
+    if len(num) != 0:
+        print('Os valores passados foram:')
+        for numero in num:
+            time.sleep(0.5)
+            print(f'{numero} ', end='')
+        print()
+        time.sleep(0.5)
+        print(f'Destes {len(num)} valores, o maior é {max(num)}.')
+    else:
+        print('Não foi passado nenhum valor. Não há valor maior.')
 
 
 maior(1, 2, 3)
 maior(10, 95, 15, 47, 36)
 maior(-1, -5, -17)
+maior(6)
+maior()
 
 numeros = ()
 while True:
@@ -19,4 +32,4 @@ while True:
     if cont == 'N':
         break
 
-maior(*numeros) #forma correta de passar os valores da tupla, e não a tupla como valor
+maior(* numeros) #forma correta de passar os valores da tupla, e não a tupla como valor
